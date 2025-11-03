bc066e531d6420bd86c93d9887c75fdd9aab6c38
OronAvisrur <oronavisrur@gmail.com>
2025-11-03 09:41:42 +0200

    feat(domain): export value objects for clean imports

---
+++ src/domain/value_objects/__init__.py
@@ -0,0 +1,35 @@
+from .identifiers import (
+    ProductId,
+    UserId,
+    ConversationId,
+    MessageId,
+    Identifier,
+    ProductIdentifier,
+    UserIdentifier,
+    ConversationIdentifier,
+    MessageIdentifier,
+)
+from .entities import (
+    IntentType,
+    EntityType,
+    Entity,
+    PriceRange,
+    MessageRole,
+)
+
+__all__ = [
+    "ProductId",
+    "UserId",
+    "ConversationId",
+    "MessageId",
+    "Identifier",
+    "ProductIdentifier",
+    "UserIdentifier",
+    "ConversationIdentifier",
+    "MessageIdentifier",
+    "IntentType",
+    "EntityType",
+    "Entity",
+    "PriceRange",
+    "MessageRole",
+]
