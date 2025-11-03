48dad6af1b5e7f5cd3da69e115d61e6f7f9b2874
OronAvisrur <oronavisrur@gmail.com>
2025-11-03 09:52:55 +0200

    feat(domain): export all repository protocols

---
+++ src/domain/repositories/__init__.py
@@ -0,0 +1,9 @@
+from .product_repository import ProductRepository
+from .conversation_repository import ConversationRepository
+from .user_repository import UserRepository
+
+__all__ = [
+    "ProductRepository",
+    "ConversationRepository",
+    "UserRepository",
+]
