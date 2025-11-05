d536ab9a30b72e27b243fc3673668241502864aa
OronAvisrur <oronavisrur@gmail.com>
2025-11-05 17:24:58 +0200

    feat: export VectorRepository from repositories package
    - Add VectorRepository to imports and __all__ exports
    - Make protocol available for import from domain.repositories

--- backend/src/domain/repositories/__init__.py
+++ backend/src/domain/repositories/__init__.py
@@ -2,10 +2,12 @@ from .conversation_repository import ConversationRepository
 from .product_repository import ProductRepository
 from .user_repository import UserRepository
 from .embedding_repository import EmbeddingRepository
+from .vector_repository import VectorRepository
 
 __all__ = [
     "ConversationRepository",
     "ProductRepository",
     "UserRepository",
     "EmbeddingRepository",
+    "VectorRepository",
 ]
