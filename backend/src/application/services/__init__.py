dc9f68f930a468838b0aa3480f4924ac41cfee0d
OronAvisrur <oronavisrur@gmail.com>
2025-11-06 12:03:52 +0200

    feat: export ProductIngestionService from services package
    - Add ProductIngestionService to exports
    - Make ingestion service available from application.services

--- backend/src/application/services/__init__.py
+++ backend/src/application/services/__init__.py
@@ -1,9 +1,11 @@
 from .intent_detector import IntentDetectorService
 from .text_chunker import TextChunker, TextChunk, ChunkConfig
+from .product_ingestion import ProductIngestionService
 
 __all__ = [
     "IntentDetectorService",
     "TextChunker",
     "TextChunk",
     "ChunkConfig",
+    "ProductIngestionService",
 ]
